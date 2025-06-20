1a01f7b665c89bf566a6861b40e040b8ad5b51cf
Thomas Melak <me@thomasmelak.io>
2025-06-21 00:50:09 +0300

    Update to google maps

--- src/components/pages/hubs/about-map/AboutMapContainer.tsx
+++ src/components/pages/hubs/about-map/AboutMapContainer.tsx
@@ -1,15 +1,21 @@
 import { AboutMapPresentation } from '.'
 
-type Branch = { id: string; position: [number, number]; description: string }
+export type Branch = { id: string; position: [number, number]; description: string }
 
 const AboutMapContainer = () => {
   const branches: Branch[] = [
+    // {
+    //   id: '1',
+    //   position: [9.03333, 38.7],
+    //   description: 'Creative Hub Mexico',
+    // },
     {
       id: '1',
-      position: [9.03333, 38.7],
-      description: 'Creative Hub Mexico',
+      position: [9.032859419869673, 38.74998989472767],
+      description: 'Creative Hub Addis Ababa',
     },
   ]
+
   return <AboutMapPresentation branches={branches} />
 }
 
--- src/components/pages/hubs/about-map/AboutMapPresentation.tsx
+++ src/components/pages/hubs/about-map/AboutMapPresentation.tsx
@@ -1,62 +1,49 @@
 'use client'
 
-import type { LatLngTuple } from 'leaflet'
-import dynamic from 'next/dynamic'
-import type { MapContainerProps, MarkerProps, TileLayerProps } from 'react-leaflet'
+import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps'
+// import dynamic from 'next/dynamic'
+import type { Branch } from './AboutMapContainer'
 
-const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), {
-  ssr: false,
-}) as (typeof import('react-leaflet'))['MapContainer']
+// const MapContainer = dynamic(() => import('react-leaflet').then((mod) => mod.MapContainer), {
+//   ssr: false,
+// }) as (typeof import('react-leaflet'))['MapContainer']
 
-const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {
-  ssr: false,
-}) as (typeof import('react-leaflet'))['TileLayer']
+// const TileLayer = dynamic(() => import('react-leaflet').then((mod) => mod.TileLayer), {
+//   ssr: false,
+// }) as (typeof import('react-leaflet'))['TileLayer']
 
-const Marker = dynamic(() => import('react-leaflet').then((mod) => mod.Marker), {
-  ssr: false,
-}) as (typeof import('react-leaflet'))['Marker']
+// const Marker = dynamic(() => import('react-leaflet').then((mod) => mod.Marker), {
+//   ssr: false,
+// }) as (typeof import('react-leaflet'))['Marker']
 
-const Popup = dynamic(() => import('react-leaflet').then((mod) => mod.Popup), {
-  ssr: false,
-}) as (typeof import('react-leaflet'))['Popup']
+// const Popup = dynamic(() => import('react-leaflet').then((mod) => mod.Popup), {
+//   ssr: false,
+// }) as (typeof import('react-leaflet'))['Popup']
 
-type Branch = { id: string; position: [number, number]; description: string }
+// type Branch = { id: string; position: [number, number]; description: string }
 
 interface AboutMapPresentationProps {
   branches: Branch[]
 }
 
 export default function AboutMapPresentation({ branches }: AboutMapPresentationProps) {
-  const center: LatLngTuple = branches[0]?.position ?? [0, 0]
-
-  const mapProps: MapContainerProps = {
-    center,
-    zoom: 15,
-    style: { height: '100%', width: '100%' },
-    scrollWheelZoom: false,
-  }
-
-  const tileLayerProps: TileLayerProps = {
-    url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }
-
-  const markerProps: MarkerProps = {
-    position: center,
-  }
+  const API_KEY = 'AIzaSyCzaXHlqPCHFVf-APQotB98t1OJjA5nZG0'
+  //  || process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 
   return (
-    <section className="md:px-8 px-4 py-12">
+    <section className="md:px-8 px-4 py-12 max-h-[80vh] overflow-hidden">
       <div className="aspect-square w-full overflow-hidden rounded-2xl  md:aspect-banner">
-        <MapContainer {...mapProps}>
-          <TileLayer {...tileLayerProps} />
-          {branches.map((branch) => (
-            <Marker {...markerProps} key={branch.id} position={branch.position}>
-              <Popup>{branch.description} </Popup>
-            </Marker>
-          ))}
-        </MapContainer>
+        <APIProvider apiKey={API_KEY}>
+          <Map
+            style={{ width: '100vw', height: '100vh' }}
+            defaultCenter={{ lat: 9.024914681988621, lng: 38.759114460381866 }}
+            defaultZoom={15}
+            gestureHandling={'none'}
+            disableDefaultUI={true}
+          >
+            <Marker position={{ lat: 9.03285, lng: 38.74998 }} />
+          </Map>
+        </APIProvider>
       </div>
     </section>
   )
